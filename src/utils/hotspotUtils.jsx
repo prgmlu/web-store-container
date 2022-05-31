5113443b8c9a84609968211f0f6f10d7161b025d
Mike <michael@obsessvr.com>
2022-05-31 16:04:54 +0000

    adding hover and unhover hotspot

---
+++ src/utils/hotspotUtils.jsx
@@ -0,0 +1,126 @@
+export const ICON_NAMES = Object.freeze({
+	product: {
+		default: 'product_hotspot_icon',
+		hover: 'product_hotspot_icon_hover',
+	},
+	image: {
+		default: 'image_hotspot_icon',
+		hover: 'image_hotspot_icon_hover',
+	},
+	video: {
+		default: 'video_hotspot_icon',
+		hover: 'video_hotspot_icon_hover',
+	},
+	video_with_button: {
+		default: 'video_hotspot_icon',
+		hover: 'video_hotspot_icon_hover',
+	},
+	text_with_button: {
+		default: 'text_hotspot_icon',
+		hover: 'text_hotspot_icon_hover',
+	},
+	youtube_embed: {
+		default: 'youtube_hotspot_icon',
+		hover: 'youtube_hotspot_icon_hover',
+	},
+	custom: {
+		// icon not yet defined in COCO
+		default: '',
+		hover: '',
+	},
+	zoom: {
+		// icon not yet defined in COCO
+		default: '',
+		hover: '',
+	},
+	sound: {
+		default: 'sound_hotspot_icon',
+		hover: 'sound_hotspot_icon_hover',
+	},
+	carousel: {
+		// icon not yet defined in COCO
+		default: '',
+		hover: '',
+	},
+	image_carousel: {
+		default: 'image_carousel_hotspot_icon',
+		hover: 'image_carousel_hotspot_icon_hover',
+	},
+	quiz: {
+		default: 'quiz_hotspot_icon',
+		hover: 'quiz_hotspot_icon_hover',
+	},
+	link: {
+		default: 'link_hotspot_icon',
+		hover: 'link_hotspot_icon_hover',
+	},
+	iframe: {
+		default: 'iframe_hotspot_icon',
+		hover: 'iframe_hotspot_icon_hover',
+	},
+});
+
+// mock tests for different hotspot styling configs
+// replace stylingIcons in RoomObjects.jsx
+export const stylingIconsMock = {
+	product_hotspot_icon: {
+		name: 'play_black',
+	},
+	product_hotspot_icon_hover: {
+		name: 'dot_with_ring_white',
+	},
+	image_hotspot_icon: {
+		name: 'play_black',
+	},
+	image_hotspot_icon_hover: {
+		name: 'dot_with_ring_white',
+	},
+	video_hotspot_icon: {
+		name: 'play_black',
+	},
+	video_hotspot_icon_hover: {
+		name: 'dot_with_ring_white',
+	},
+	text_hotspot_icon: {
+		name: 'play_black',
+	},
+	text_hotspot_icon_hover: {
+		name: 'dot_with_ring_white',
+	},
+	youtube_embed_icon: {
+		name: 'play_black',
+	},
+	youtube_embed_icon_hover: {
+		name: 'dot_with_ring_white',
+	},
+	sound_hotspot_icon: {
+		name: 'play_black',
+	},
+	sound_hotspot_icon_hover: {
+		name: 'dot_with_ring_white',
+	},
+	image_carousel_hotspot_icon: {
+		name: 'play_black',
+	},
+	image_carousel_hotspot_icon_hover: {
+		name: 'dot_with_ring_white',
+	},
+	quiz_hotspot_icon: {
+		name: 'play_black',
+	},
+	quiz_hotspot_icon_hover: {
+		name: 'dot_with_ring_white',
+	},
+	link_hotspot_icon: {
+		name: 'play_black',
+	},
+	link_hotspot_icon_hover: {
+		name: 'dot_with_ring_white',
+	},
+	iframe_hotspot_icon: {
+		name: 'play_black',
+	},
+	iframe_hotspot_icon_hover: {
+		name: 'dot_with_ring_white',
+	},
+};
