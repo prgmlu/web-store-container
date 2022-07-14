97f471c04d2dcc11bccc69ef897e84e22c6b264e
Yuliang Xue <yuliang@obsessvr.com>
2022-07-14 21:11:41 +0000

    Feature/6153

--- src/redux_stores/localizeReducer/actions.jsx
+++ src/redux_stores/localizeReducer/actions.jsx
@@ -1,5 +1,6 @@
 import { createAction } from '@reduxjs/toolkit';
-import { INITIALIZE_LOCALIZE, SET_TRANSLATIONS_FOR_LANG } from './types';
+import { INITIALIZE_LOCALIZE, SET_TRANSLATIONS_FOR_LANG, SET_ACTIVE_LOCALE } from './types';
 
 export const initialize = createAction(INITIALIZE_LOCALIZE);
 export const setTranslationsForLang = createAction(SET_TRANSLATIONS_FOR_LANG);
+export const setActiveLocale = createAction(SET_ACTIVE_LOCALE);
