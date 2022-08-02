4a48557f75baedea335a611a9cff3e8122458429
yuliangxue <yuliang@obsessvr.com>
2022-08-02 11:43:59 -0400

    added on close to entrance video

--- src/components/Room/EntranceVideo.jsx
+++ src/components/Room/EntranceVideo.jsx
@@ -1,6 +1,6 @@
 import React from 'react';
 
-const EntranceVideo = ({ videoUrl, onVideoEnd }) => {
+const EntranceVideo = ({ videoUrl, onVideoEnd, onClose }) => {
 	return (
 		<video
 			className="h-100 w-100 entrance-video"
@@ -10,6 +10,8 @@ const EntranceVideo = ({ videoUrl, onVideoEnd }) => {
 			playsInline
 			style={{ objectFit: 'cover' }}
 			onEnded={onVideoEnd}
+			onClick={onClose}
+			onTouchEnd={onClose}
 		>
 			<source src={videoUrl} />
 		</video>
