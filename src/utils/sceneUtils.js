e2dd4515ac96bbf736c4ce967dec150df6b4ed53
Yashodhan Kumthekar <yashodhan@obsessvr.com>
2022-08-26 10:18:12 -0400

    Updates

--- src/utils/sceneUtils.js
+++ src/utils/sceneUtils.js
@@ -2,8 +2,8 @@
 export const getSceneType = (sceneData) => {
 	// not all scenes have scene_type prop as it was only recently added
 	let sceneType = sceneData?.scene_type;
-	if (!sceneType && (sceneData?.flat_scene_url || sceneData?.image))
+	if (!sceneType && sceneData?.cube_map_dir) sceneType = 'cubemap_scene';
+	else if (!sceneType && (sceneData?.flat_scene_url || sceneData?.image))
 		sceneType = 'flat_scene';
-	else if (!sceneType && sceneData?.cube_map_dir) sceneType = 'cubemap_scene';
 	return sceneType;
 };
